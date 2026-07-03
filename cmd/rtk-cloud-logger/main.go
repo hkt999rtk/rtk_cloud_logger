d513b0c6fc70e7237c2f1a7ddeb538200ad54d12
Kevin Huang <hkt999@gmail.com>
2026-07-03 22:48:32 +0800

    Require Loki-backed cloud logger storage

--- cmd/rtk-cloud-logger/main.go
+++ cmd/rtk-cloud-logger/main.go
@@ -12,30 +12,16 @@ import (
 func main() {
 	addr := flag.String("addr", ":18090", "listen address")
 	token := flag.String("token", os.Getenv("RTK_CLOUD_LOGGER_TOKEN"), "bearer token required by forwarders")
-	storeKind := flag.String("store", firstNonEmpty(os.Getenv("RTK_CLOUD_LOGGER_STORE"), "memory"), "event store: memory or loki")
 	lokiURL := flag.String("loki-url", os.Getenv("RTK_CLOUD_LOGGER_LOKI_URL"), "Loki base URL")
 	flag.Parse()
 
-	store := cloudlogger.EventStore(cloudlogger.NewMemoryEventStore())
-	if *storeKind == "loki" {
-		loki, err := cloudlogger.NewLokiEventStore(cloudlogger.LokiStoreConfig{BaseURL: *lokiURL})
-		if err != nil {
-			log.Fatal(err)
-		}
-		store = loki
+	store, err := cloudlogger.NewLokiEventStore(cloudlogger.LokiStoreConfig{BaseURL: *lokiURL})
+	if err != nil {
+		log.Fatal(err)
 	}
 	handler := cloudlogger.IngestHandler(store, cloudlogger.IngestConfig{Token: *token})
 	log.Printf("starting rtk-cloud-logger on %s", *addr)
 	if err := http.ListenAndServe(*addr, handler); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func firstNonEmpty(values ...string) string {
-	for _, value := range values {
-		if value != "" {
-			return value
-		}
-	}
-	return ""
 }
